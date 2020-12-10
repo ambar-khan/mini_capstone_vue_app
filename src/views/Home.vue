b01d429d15b22017884af6d354e1f42fe7d9866f
Ambar <ambarmogg@me.com>
2020-12-09 19:35:36 -0800

    index action pulling from api with axios

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,17 +9,27 @@
 <style>
 </style>
 <script>
+import axios from "axios";
+
 export default {
   data: function () {
     return {
       message: "Products Page",
-      products: [
-        { id: 1, name: "Product 1", price: 10 },
-        { id: 2, name: "Product 2", price: 230 },
-      ],
+      products: [],
     };
   },
-  created: function () {},
-  methods: {},
+  created: function () {
+    console.log("in created");
+    this.productsIndex();
+  },
+  methods: {
+    productsIndex: function () {
+      console.log("products index..");
+      axios.get("/api/products").then((response) => {
+        console.log(response.data);
+        this.products = response.data;
+      });
+    },
+  },
 };
 </script>
