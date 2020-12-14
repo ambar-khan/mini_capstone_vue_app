6aaae1318212acf5f36fd8586712175bba42c357
Ambar <ambarmogg@me.com>
2020-12-13 21:20:09 -0800

    hardcoded new/create action

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <button v-on:click="createProduct()">Add Product</button>
+    
     <div v-for="product in products">
-      {{ product.name }}
-      <p><img v-bind:src="product.image_url"v-bind:alt="product.title"></p>
+      {{ product }}
+      <p><img v-bind:src="product.image_url" v-bind:alt="product.title"></p>
     </div>
   </div>
 </template>
@@ -29,6 +31,21 @@ export default {
       axios.get("/api/products").then((response) => {
         console.log(response.data);
         this.products = response.data;
+      });
+    },
+    createProduct: function () {
+      console.log("creating product...");
+      var params = {
+        name: "Baby Yoda",
+        description: "Baby Yoda doll",
+        image_url: "the image url",
+        price: 45,
+        tax: 5,
+        total: 50,
+      };
+      axios.post("/api/products", params).then((response) => {
+        console.log(response.data);
+        this.products.push(response.data);
       });
     },
   },
